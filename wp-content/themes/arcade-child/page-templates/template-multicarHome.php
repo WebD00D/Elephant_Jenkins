7f665932b91de6706353961d2885b71ea51013a2
Christian <christian@stabmag.com>
2018-08-08 07:47:00 -0700

    testing again

--- wp-content/themes/arcade-child/page-templates/template-multicarHome.php
+++ wp-content/themes/arcade-child/page-templates/template-multicarHome.php
@@ -70,7 +70,7 @@ CUSTOM FIELD LIST
             <!-- https://quotes.elephant.com/#/postal-landing/23233 -->
         </div>
         <div class="js_header_error error-box d-none">
-            Please enter a valid zipcode...
+            Please enter a valid zipcode, please.
         </div>
         <div class="t-center--sm">
             <a class="retrieve-quote td-none" href="https://quotes.elephant.com/#/quote-retrieve">Retrieve <span>Saved Quote</span></a>
